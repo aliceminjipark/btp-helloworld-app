dfe0bb47ac9b72425c6df8ba2d97c14cbcf75546
Minji Park <alice.park@vision33.com>
2025-02-28 00:34:46 +0000

    first commit

---
+++ webapp/controller/View1.controller.ts
@@ -0,0 +1,18 @@
+import Controller from "sap/ui/core/mvc/Controller";
+import MessageBox from "sap/m/MessageBox";
+
+
+/**
+ * @namespace sap.btp.helloworldui5.controller
+ */
+export default class View1 extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+
+    public onPress(): void {
+        MessageBox.alert("You have been alerted!");
+    }
+}
